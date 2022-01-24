bec0edd487269607dd512bcbb9f3d94889c3f839
myairin <irenewindagultom@gmail.com>
2022-01-24 10:01:15 +0700

    perubahan dua

--- Angka-Ganjil-Genap/angkaGanjilGenap.js
+++ Angka-Ganjil-Genap/angkaGanjilGenap.js
@@ -1,7 +1,48 @@
-for(let i = 1; i <= 100; i+=3){
-    if(i % 2 === 0) {
-        console.log(i + " -genap")
-    } if(i % 2 !== 1) {
-        console.log(i + " -ganjil")
+/* SOAL PERTAMA */
+
+// for(let i = 1; i <= 100; i+=3) {
+//     if(i % 2 === 0) {
+//         console.log(i + "  = genap")
+//     } if(i % 2 !== 0) 
+//     {
+//         console.log(i + " = ganjil")
+//     }
+// } ;
+
+/* SOAL KEDUA*/ 
+
+// for(let i = 50; i <= 200 ; i+=5) {
+//     if(i % 3 === 0) {
+//         console.log(i + "  = genap")
+//     } 
+// } ;
+
+
+/* SOAL KETIGA */
+
+// for(let i = 100; i <= 200; i+=7) {
+//     if(i % 8 === 0) {
+//         console.log(i + "  = genap")
+//     } 
+// } ;
+
+// r
+// var kalimat = "reza is so cool";
+// var hurufS = kalimat[13];
+
+// console.log(hurufS + " = ini huruf o");
+
+
+
+
+//soal 
+
+let baris = 4;
+let bintang =''
+
+for (let i = 0; i < baris; i++) {
+    console.log('loop satu', i);
+    for (let j = 0; j < 4; j++){
+        console.log('loop dua', j);
     }
-}
+} ;
